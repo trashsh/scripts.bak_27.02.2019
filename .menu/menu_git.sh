06cf7e88d72c2d75d416b04ca41c20fe7bb2db74
Gothundead <r@gothundead.ru>
2019-01-25 15:49:59 +0300

    25/01/2019 15:49:28

--- .menu/menu_git.sh
+++ .menu/menu_git.sh
@@ -4,9 +4,10 @@ source ~/.bashrc
 echo ''
 echo -e "${COLOR_GREEN} ===Управление Git===${COLOR_NC}"
 
-echo '1: Push remote'
+echo '1: Git commit'
+echo '2: Push remote'
 echo '2: Git remote view'
-echo '3: Git commit'
+
 
 echo '0: Назад'
 echo '/: Выход'
@@ -14,12 +15,13 @@ echo ''
 echo -n 'Выберите пункт меню:'
 read item
 case "$item" in
-        1) $SCRIPTS/git/git_remote_push.sh $1
+		1) $SCRIPTS/git/git_commit.sh $1
             ;;
-        2) $SCRIPTS/git/git_remote_view.sh $1
+        2) $SCRIPTS/git/git_remote_push.sh $1
             ;;
-		3) $SCRIPTS/git/git_commit.sh $1
+        3) $SCRIPTS/git/git_remote_view.sh $1
             ;;
+		
         0)  echo ''
             $MYFOLDER/scripts/menu $1
             ;;
