f3f17a721151b8ef9123c5e0e038ba2dd3931e9b
Gothundead <r@gothundead.ru>
2019-01-31 16:28:37 +0300

    31/01/2019 16:28:37

--- git/git_remote_push.sh
+++ git/git_remote_push.sh
@@ -7,9 +7,9 @@ source ~/.bashrc
 clear
 echo -e "\n ${COLOR_YELLOW} Push to github.com & Bitbucket.org:${COLOR_NC}"
 git remote -v
-sudo git remote add origin https://trashsh@github.com/trashsh/myserv.git
-sudo git remote add bitbucket https://gothundead@bitbucket.org/gothundead/myserv.git
-sudo git push origin master
+sudo git remote add github https://trashsh@github.com/trashsh/scripts.git
+sudo git remote add bitbucket https://gothundead@bitbucket.org/gothundead/scripts.git
+sudo git push github master
 sudo git push bitbucket master
 echo ""
 $SCRIPTS/.menu/menu_git.sh $1
