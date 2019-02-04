d44d88dac621d42a9a9ca5f40a3361b25a3bcdec
Gothundead <r@gothundead.ru>
2019-02-04 13:55:26 +0300

    04/02/2019 13:55:26

--- users/make/mysql.sh
+++ users/make/mysql.sh
@@ -10,34 +10,37 @@ then
 echo ''
 echo -e "$COLOR_YELLOW"Создание пользователя базы данных mysql" $COLOR_NC"
 
-		echo -n -e "Добавить пользователя $COLOR_YELLOW" $2 "$COLOR_NC? пользователем баз данных mysql? \nВведите $COLOR_BLUE\"y\"$COLOR_NC для подтверждения, для выхода - $COLOR_BLUEлюбой символ$COLOR_NC: "
+		echo -n -e "Добавить пользователя $COLOR_YELLOW" $2 "$COLOR_NC? пользователем баз данных mysql? \nВведите $COLOR_BLUE\"y\"$COLOR_NC для подтверждения, для выхода - $COLOR_BLUE\"n\"$COLOR_NC: "
 		
 		
-		read item
-		case "$item" in
+		while read
+		do
+			echo -n ": "
+			case "$REPLY" in
 			y|Y) echo
 				#.my.cnf
 
-				touch $HOMEPATHWEBUSERS/$2/.my.cnf		
-				
-				
-				
+				touch $HOMEPATHWEBUSERS/$2/.my.cnf	
 				echo -n -e "$COLOR_BLUE Введите пароль для пользователя$COLOR_NC $COLOR_YELLOW" $2 "$COLOR_NC $COLOR_BLUEбазы данных mysql$COLOR_NC:"
-				read -s PASSWORD
-				echo ""				
-				echo -n -e "Пользователь $COLOR_YELLOW" $2 "$COLOR_NC имеет набор прав пользователя или администратора сервера? Введите $COLOR_BLUE\"1\"$COLOR_NC, если набор прав пользователя, $COLOR_BLUE\"2\"$COLOR_NC если администратора: "
-		read item
-		case "$item" in
-			1) echo							
-				$SCRIPTS/mysql/make/useradd_make_user.sh $1 $2 $PASSWORD			
-				;;
-			2) 
-			echo 'Отмена операции добавления пользователя баз данных mysql'
-			$SCRIPTS/mysql/make/useradd_make_root.sh $1 $2 $PASSWORD
-			echo ''
-            ;;
-		esac
-		
+				read -s PASSWORD				
+					break;;
+			n|N)  exit 0;
+			esac
+		done
+
+		echo ""				
+				echo -n -e "Пользователь $COLOR_YELLOW" $2 "$COLOR_NC имеет набор прав пользователя или администратора сервера? Введите $COLOR_BLUE\"y\"$COLOR_NC, если набор прав пользователя, $COLOR_BLUE\"n\"$COLOR_NC если администратора: "
+		while read
+		do
+			echo -n ": "
+			case "$REPLY" in
+			y|Y) $SCRIPTS/mysql/make/useradd_make_user.sh $1 $2 $PASSWORD	
+					break;;
+			n|N) $SCRIPTS/mysql/make/useradd_make_root.sh $1 $2 $PASSWORD
+					break;;
+			esac
+		done		
+
 		
 			if [ -f "$HOMEPATHWEBUSERS"/"$2"/".my.cnf" ] ; then
 				   cat $HOMEPATHWEBUSERS/$2/.my.cnf | grep $HOMEPATHWEBUSERS
@@ -51,11 +54,7 @@ echo -e "$COLOR_YELLOW"Создание пользователя базы дан
 			fi
 							
 
-				;;
-        *) echo 'Отмена операции добавления пользователя'
-			echo ''
-            ;;
-    esac
+	
 
 else
     echo -e "\n$COLOR_YELLOWПараметры запуска не найдены$COLOR_NC. Необходимы параметры: имя пользователя"
