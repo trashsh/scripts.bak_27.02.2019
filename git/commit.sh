b06028e705dd9f90935688f15e3fff0df088fd79
root <root@alixi.ru>
2019-02-06 02:11:04 +0300

    06/02/2019 02:11:04

--- git/commit.sh
+++ git/commit.sh
@@ -26,3 +26,4 @@ while read
         esac
     done
 
+exit 0
--- git/remote_push.sh
+++ git/remote_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#просмотр удаленных репозиториев
+#публикация удаленного репозитария
 #$1-$USERNAME process;
 source /etc/profile
 source ~/.bashrc
@@ -7,10 +7,10 @@ source ~/.bashrc
 clear
 cd $SCRIPTS
 echo -e "\n${COLOR_YELLOW}Публикация репозитария на github.com и Bitbucket.org:${COLOR_NC}"
-git remote -v
 sudo git remote add github https://trashsh@github.com/trashsh/scripts.git
 sudo git remote add bitbucket https://gothundead@bitbucket.org/gothundead/scripts.git
 sudo git push github master
 sudo git push bitbucket master
 echo ""
 $SCRIPTS/.menu/git.sh $1
+exit 0
