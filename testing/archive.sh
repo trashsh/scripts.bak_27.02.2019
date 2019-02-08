53c50bfcd4681ea7edc75d2fba9023a5c0eef676
Gothundead <r@gothundead.ru>
2019-02-08 11:22:51 +0300

    08/02/2019 11:22:51

--- testing/archive.sh
+++ testing/archive.sh
@@ -3,5 +3,5 @@ source /etc/profile
 source ~/.bashrc
 source $SCRIPTS/functions/archive.sh
 
-tar_folder_structure "/my/1/" "/my/back1/2.tar.gz"
+untar_folder_with_structure "/my/1/фыва" "/3/w343w4/archive.tar.gz"
 echo "12323"
