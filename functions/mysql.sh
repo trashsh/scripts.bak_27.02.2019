5971275e5c035c736b37d2b319b4fc7e8b171a8d
Gothundead <r@gothundead.ru>
2019-02-11 11:31:21 +0300

    11/02/2019 11:31:21

--- functions/mysql.sh
+++ functions/mysql.sh
@@ -2,13 +2,12 @@
 #Функции mysql
 source $SCRIPTS/functions/archive.sh
 declare -x -f dbBackupBases	#Создание бэкапа всех пользовательских баз данных. #В параметре $1 может быть установлен каталог выгрузки. По умолчанию грузится в `date +%Y.%m.%d`
-declare -x -f dbBackupBase
-declare -x -f dbShowGrants
-declare -x -f dbBaseIsExist
+declare -x -f dbBackupBase	#Создание бэкапа указанной базы данных. #$1 - название базы данных. В параметре $2 может быть установлен каталог выгрузки. По умолчанию грузится в $BACKUPFOLDER_DAYS\`date +%Y.%m.%d`
+declare -x -f dbCheckExportedBase #проверка успешности выгрузки базы данных mysql. $1-имя базы; $2-имя проверяемого файла
 
+#######СДЕЛАНО. Не трогать!!!!#######
 #Создание бэкапа всех пользовательских баз данных.
 #В параметре $1 может быть установлен каталог выгрузки. По умолчанию грузится в $BACKUPFOLDER_DAYS\`date +%Y.%m.%d`
-#tested
 dbBackupBases(){
 #	mysql -e "show databases;"
 	d=`date +%Y.%m.%d`;
@@ -53,12 +52,12 @@ dbBackupBases(){
 	for db in $databases; do
 		if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "phpmyadmin" ]] && [[ "$db" != "sys" ]] ; then
 			echo -e "---\nВыгрузка базы данных MYSQL: ${COLOR_YELLOW}$db${COLOR_NC}"
-			mysqldump --databases $db > $DESTINATION/$db.$dt.sql
+			mysqldump --databases $db > $DESTINATION/$db_$dt.sql
 #архивация выгруженной базы и удаление оригинального файла sql
-			tar_file_without_structure_remove	$DESTINATION/$db.$dt.sql $DESTINATION/$db.$dt.tar.gz	
+			tar_file_without_structure_remove	$DESTINATION/$db_$dt.sql $DESTINATION/$db_$dt.tar.gz	
 #проверка на существование выгруженных и заархививанных баз данных
-			if  [ -f "$DESTINATION/$db.$dt.tar.gz" ] ; then
-				echo -e "${COLOR_GREEN}Выгрузка базы данных MYSQL:${COLOR_NC} ${COLOR_YELLOW}$db${COLOR_NC} ${COLOR_GREEN}успешно завершилась в файл${COLOR_NC}${COLOR_YELLOW} \"$DESTINATION/$db.$dt.tar.gz\"${COLOR_NC}\n---"
+			if  [ -f "$DESTINATION/$db_$dt.tar.gz" ] ; then
+				echo -e "${COLOR_GREEN}Выгрузка базы данных MYSQL:${COLOR_NC} ${COLOR_YELLOW}$db${COLOR_NC} ${COLOR_GREEN}успешно завершилась в файл${COLOR_NC}${COLOR_YELLOW} \"$DESTINATION/$db_$dt.tar.gz\"${COLOR_NC}\n---"
 			else
 				echo -e "${COLOR_RED}Выгрузка базы данных: ${COLOR_NC}${COLOR_YELLOW}$db${COLOR_NC} ${COLOR_RED}завершилась с ошибкой${COLOR_NC}\n---"
 			fi			
@@ -67,55 +66,81 @@ done
 }
 
 #--------------------------------------------------------------------------------------------------------------------------------
-
-##НЕ СДЕЛАНО!!!!
+#######СДЕЛАНО. Не трогать!!!!#######
 #Создание бэкапа указанной базы данных.
 #$1 - название базы данных. В параметре $2 может быть установлен каталог выгрузки. По умолчанию грузится в $BACKUPFOLDER_DAYS\`date +%Y.%m.%d`
 dbBackupBase(){
-#	mysql -e "show databases;"
+	#	mysql -e "show databases;"
 	d=`date +%Y.%m.%d`;
 	dt=`date +%Y.%m.%d_%H.%M`;
-#проверка существование каталога назначения и его создание при необходимости		
-#проверка существования переданного параметра (название базы)
-	DESTINATION=/my/2
-				
-						echo -e "---\nВыгрузка базы данных MYSQL: ${COLOR_YELLOW}$1${COLOR_NC}"
-						mysqldump $1 > $DESTINATION/$1.$dt.sql
-			#архивация выгруженной базы и удаление оригинального файла sql
-						tar_file_without_structure_remove	$DESTINATION/$1.$dt.sql $DESTINATION/$1.$dt.tar.gz	
-			#проверка на существование выгруженных и заархививанных баз данных
-						if  [ -f "$DESTINATION/$1.$dt.tar.gz" ] ; then
-							echo -e "${COLOR_GREEN}Выгрузка базы данных MYSQL:${COLOR_NC} ${COLOR_YELLOW}$1${COLOR_NC} ${COLOR_GREEN}успешно завершилась в файл${COLOR_NC}${COLOR_YELLOW} \"$DESTINATION/$1.$dt.tar.gz\"${COLOR_NC}\n---"
-						else
-							echo -e "${COLOR_RED}Выгрузка базы данных: ${COLOR_NC}${COLOR_YELLOW}$1${COLOR_NC} ${COLOR_RED}завершилась с ошибкой${COLOR_NC}\n---"
-						fi			
-
-	
-}
-
-
-
-dbShowGrants(){
-if [ -n "$1" ] 
-then
-	dbBaseIsExist $1
-	if (dbexist==='1')
+	if [ -n "$1" ]
 	then
-		echo "true"		
+	
+	#проверка существования базы данных, переданной в параметре $1
+	if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$1'" 2>&1`" ]];
+	then
+	 #база $1- существует	  
+	  
+	  #проверка передачи параметра $2 с путем сохранения бэкапа
+		if [ -n "$2" ]
+		then
+		#передан путь сохраненя бэкапа
+			DESTINATION=$2
+		else
+		#путь сохранения бэкапа не передан
+			DESTINATION=$BACKUPFOLDER_DAYS/$d
+			mkdir -p $BACKUPFOLDER_DAYS/$d
+		fi		
+		
+		#пусть к файлу с бэкапом без расширения
+		FILENAME=$DESTINATION/$1_$dt
+
+		#проверка существования папки для сохранения бэкапа
+		if [ -d "$DESTINATION" ] ; then
+				mysqldump --databases $1 > $FILENAME.sql				
+				tar_file_without_structure_remove $FILENAME.sql $FILENAME.tar.gz
+				dbCheckExportedBase $1 $FILENAME.tar.gz
+		#Если нет каталога назначения	
+		else
+			echo -e "${COLOR_RED} Каталог ${COLOR_YELLOW}\"$DESTINATION\"${COLOR_NC}${COLOR_RED} не найден ${COLOR_NC}. Создать его?"	
+			echo -n -e "Введите $COLOR_BLUE\"y\"$COLOR_NC для создания каталога \"$DESTINATION\", для отмены операции - $COLOR_BLUE\"n\"$COLOR_NC: "
+		
+			while read
+			do
+			echo -n ": "
+				case "$REPLY" in
+				y|Y) 
+					mkdir -p $DESTINATION;
+					mysqldump --databases $1 > $FILENAME.sql				
+					tar_file_without_structure_remove $FILENAME.sql $FILENAME.tar.gz
+					dbCheckExportedBase $1 $FILENAME.tar.gz
+					break;;
+				n|N)
+					 break;;
+				esac	
+			done		
+		fi	
+		
 	else
-		echo "false"
+		#название базы данных не передано в параметрах
+		echo -e "${COLOR_RED}Ошибка создания бэкапа базы данных ${COLOR_YELLOW}\"$1\"${COLOR_NC}${COLOR_RED}. Указанная база не существует${COLOR_NC}"
+	fi	
+	
+	#конец проверки на наличие переданного параметра имени базы
+	else
+	 #база $1 - не существует
+	  echo -e "${COLOR_RED}В функцию ${COLOR_YELLOW}\"dbBackupBase\"${COLOR_NC}${COLOR_RED} не передано название базы данных${COLOR_NC}"
 	fi
-fi
+	#конец проверки существования базы		
 }
 
-
-
-
-dbBaseIsExist(){
-if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$1'" 2>&1`" ]];
-then
- dbexist=1
-else
- dbexist=2
-fi
+#######СДЕЛАНО. Не трогать!!!!#######
+#проверка успешности выгрузки базы данных mysql $1 в файл $2
+#$1-имя базы; $2-имя файла
+dbCheckExportedBase(){
+	if  [ -f "$2" ] ; then
+				echo -e "${COLOR_GREEN}Выгрузка базы данных MYSQL:${COLOR_NC} ${COLOR_YELLOW}\"$1\"${COLOR_NC} ${COLOR_GREEN}успешно завершилась в файл ${COLOR_NC}${COLOR_YELLOW} \"$2\"${COLOR_NC}\n---"
+			else
+				echo -e "${COLOR_RED}Выгрузка базы данных: ${COLOR_NC}${COLOR_YELLOW}\"$1\"${COLOR_NC}${COLOR_RED} в файл ${COLOR_YELLOW}\"$2\"${COLOR_NC}${COLOR_RED} завершилась с ошибкой. Указанный файл отсутствует${COLOR_NC}\n---"
+	fi
 }
--- testing/mysql.sh
+++ testing/mysql.sh
@@ -3,5 +3,13 @@ source /etc/profile
 source ~/.bashrc
 source $SCRIPTS/functions/mysql.sh
 source $SCRIPTS/functions/archive.sh
+source /my/scripts/functions/mysql.sh
 
-dbShowGrants $1
+#if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='$1'" 2>&1`" ]];
+#then
+  #echo "DATABASE ALREADY EXISTS"
+#else
+  #echo "DATABASE DOES NOT EXIST"
+#fi
+
+dbBackupBase $1 $2
