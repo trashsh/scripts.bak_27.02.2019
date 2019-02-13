9163d462caefa563084d139c5d8e4223a6dfd795
Gothundead <r@gothundead.ru>
2019-02-13 09:07:50 +0300

    13/02/2019 09:07:50 - test

--- git/commit.sh
+++ git/commit.sh
@@ -4,16 +4,33 @@
 source /etc/profile
 source ~/.bashrc
 
+
+
+
+
 echo -n -e "Для создания коммита репозитария ${COLOR_YELLOW}\""$SCRIPTS"\"${COLOR_NC} введите ${COLOR_BLUE}\"y\"${COLOR_NC}, для выхода - ${COLOR_BLUE}\"n\"${COLOR_NC}: " 
 while read
     do
         case "$REPLY" in
         "y"|"Y")  
-			echo -e "\n ${COLOR_YELLOW} Git commit generation:${COLOR_NC}"
-			cd $SCRIPTS
+			echo -n -e "Для задания имени коммита введите ${COLOR_BLUE}\"y\"${COLOR_NC}, задания вместо имени даты-времени - введите ${COLOR_BLUE}\"любой символ\"${COLOR_NC}: " 
+while read
+    do
+        case "$REPLY" in
+        "y"|"Y")  
+			echo -n -e "${COLOR_BLUE}Введите комментарий коммита${COLOR_NC}"
+			read -p ": " comment
 			dt=$(date '+%d/%m/%Y %H:%M:%S')
 			sudo git add .
-			sudo git commit -m "$dt"			
+			sudo git commit -m "$dt - $comment"			
+			break
+			;;
+        *)  
+			echo 'Отмена создания коммита'
+				$SCRIPTS/.menu/git.sh $1
+			break;; 
+        esac
+    done			
 			break
 			;;
         "n"|"N")  
