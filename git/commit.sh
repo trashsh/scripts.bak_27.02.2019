629064953f9b8ba6f6c088e06d639a3c3cff3384
Gothundead <r@gothundead.ru>
2019-02-13 09:10:34 +0300

    13/02/2019 09:10:34

--- git/commit.sh
+++ git/commit.sh
@@ -14,23 +14,25 @@ while read
         case "$REPLY" in
         "y"|"Y")  
 			echo -n -e "Для задания имени коммита введите ${COLOR_BLUE}\"y\"${COLOR_NC}, задания вместо имени даты-времени - введите ${COLOR_BLUE}\"любой символ\"${COLOR_NC}: " 
-while read
-    do
-        case "$REPLY" in
-        "y"|"Y")  
-			echo -n -e "${COLOR_BLUE}Введите комментарий коммита${COLOR_NC}"
-			read -p ": " comment
-			dt=$(date '+%d/%m/%Y %H:%M:%S')
-			sudo git add .
-			sudo git commit -m "$dt - $comment"			
-			break
-			;;
-        *)  
-			echo 'Отмена создания коммита'
-				$SCRIPTS/.menu/git.sh $1
-			break;; 
-        esac
-    done			
+				while read
+					do
+						case "$REPLY" in
+						"y"|"Y")  
+							echo -n -e "${COLOR_BLUE}Введите комментарий коммита${COLOR_NC}"
+							read -p ": " comment
+							dt=$(date '+%d/%m/%Y %H:%M:%S')
+							sudo git add .
+							sudo git commit -m "$dt - $comment"			
+							break
+							;;
+						*)  
+							dt=$(date '+%d/%m/%Y %H:%M:%S')
+							sudo git add .
+							sudo git commit -m "$dt"			
+							$SCRIPTS/.menu/git.sh $1
+							break;; 
+						esac
+					done			
 			break
 			;;
         "n"|"N")  
