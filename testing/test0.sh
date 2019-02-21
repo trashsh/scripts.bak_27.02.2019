15f609a7ff200c8fe6af07db2d4e6f46cb9bc3b3
Gothundead <r@gothundead.ru>
2019-02-21 10:34:05 +0300

    21/02/2019 10:34:05

--- testing/test0.sh
+++ testing/test0.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 source $SCRIPTS/include/include.sh
+#source $SCRIPTS/functions/users.sh
 
 #userAddSystem
 #dbViewUserInfo $1 $2
 #echo $?
 
-#dbUseradd $1 $2 $3 $4 $5
-viewUserInGroupUsersByPartName $1 $2
-echo $?
+#createSite $1 $2 $3 $4 $5
+#viewUserInGroupUsersByPartName $1 $2
+#echo $?
 
 
+#inputSite_Laravel $1
+useraddFtp $1 $2 $3
